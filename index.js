543f95e541232f8011b194a2cc4e57f6f72bd261
YOUR NAME <jhanov@gmail.com>
2021-12-14 16:56:16 -0600

    created objects for each category

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 // let watchers = response.data["subscribers_count"];
 // let forksCount = response.data["forks"];
 
-let vue = {};
-let angular = {};
-let ember = {};
-let svelte = {};
-let react = {};
+let forks = {};
+let stars = {};
+let watchers = {};
+let labels = [];
+
 
 
 axios.get("https://api.github.com/repos/vuejs/vue").then(function (response) {
@@ -16,18 +16,57 @@ axios.get("https://api.github.com/repos/vuejs/vue").then(function (response) {
   let stars = response.data["stargazers_count"];
   let watchers = response.data["subscribers_count"];
   let forksCount = response.data["forks"];
-  vue["stars"] = stars;
-  vue["watchers"] = watchers;
-  vue["forks"] = forksCount;
-  console.log(vue);
+  forks["Vue"] = forksCount;
+  stars["Vue"] = stars;
+  watchers["Vue"] = watchers;
 });
 
+axios.get("https://api.github.com/repos/angular/angular.js").then(function (response) {
+  // console.log(response.data);
+  let stars = response.data["stargazers_count"];
+  let watchers = response.data["subscribers_count"];
+  let forksCount = response.data["forks"];
+  forks["Angular"] = forksCount;
+  stars["Angular"] = stars;
+  watchers["Angular"] = watchers;
+});
+
+axios.get("https://api.github.com/repos/emberjs/ember.js").then(function (response) {
+  // console.log(response.data);
+  let stars = response.data["stargazers_count"];
+  let watchers = response.data["subscribers_count"];
+  let forksCount = response.data["forks"];
+  forks["Ember"] = forksCount;
+  stars["Ember"] = stars;
+  watchers["Ember"] = watchers;
+});
+
+axios.get("https://api.github.com/repos/sveltejs/svelte").then(function (response) {
+  // console.log(response.data);
+  let stars = response.data["stargazers_count"];
+  let watchers = response.data["subscribers_count"];
+  let forksCount = response.data["forks"];
+  forks["Svelte"] = forksCount;
+  stars["Svelte"] = stars;
+  watchers["Svelte"] = watchers;
+});
+
+
+axios.get("https://api.github.com/repos/facebook/react").then(function (response) {
+  // console.log(response.data);
+  let stars = response.data["stargazers_count"];
+  let watchers = response.data["subscribers_count"];
+  let forksCount = response.data["forks"];
+  forks["React"] = forksCount;
+  stars["React"] = stars;
+  watchers["React"] = watchers;
+});
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['VueJS Forks', 'VueJS Stars', 'Vue JS watchers', 'Orange'],
+    labels: ['Vue Forks', 'VueJS Stars', 'Vue watchers', 'Angular Stars'],
     datasets: [{
       label: '# of People',
       data: [2, 4, 6, 5, 2, 3],
